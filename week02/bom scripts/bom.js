00d118e013522631af309ba6c3f906c36c94d2ca
Kaylee <kaylee@builtmarketing.co>
2026-05-13 20:06:03 -0500

    BOM activity

---
+++ week02/bom scripts/bom.js
@@ -0,0 +1,38 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("#addButton");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function () {
+
+    if (input.value.trim() !== "") {
+
+        const li = document.createElement("li");
+
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = "❌";
+
+        deleteButton.setAttribute(
+            "aria-label",
+            `Remove ${input.value}`
+        );
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+        });
+
+        input.value = "";
+
+        input.focus();
+
+    } else {
+        input.focus();
+    }
+
+});
